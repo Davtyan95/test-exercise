721adf4f8fa744666213808a231ce79d3b1c980e
Davtyan95 <haykdavtyan007@gmail.com>
2022-06-19 17:25:10 +0400

    fix front issues

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('permission.index')" :active="request()->routeIs('permission.index')">
+                {{ __('Permission') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('role.index')" :active="request()->routeIs('role.index')">
+                {{ __('Role') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
